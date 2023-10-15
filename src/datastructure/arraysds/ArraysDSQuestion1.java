86ef26a2dc30d5af35c21f80441b058576e3ffb2
imalkaweerasekara <imajavaweerasekara128@gmail.com>
2023-10-15 23:46:01 +0530

    Initial Commit

---
+++ src/datastructure/arraysds/ArraysDSQuestion1.java
@@ -0,0 +1,29 @@
+/*
+ * *
+ *  * Copyright (C) 2023 Udesh Imalka Weerasekara. All rights reserved.
+ *  * Senior Software Engineer
+ *
+ */
+
+package datastructure.arraysds;
+/**
+ Date:2023-10-15
+ Author:imalkaweerasekara
+ */
+
+class ArraysDSQuestion1 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * @param array
+     * @return
+     * @throws IndexOutOfBoundsException
+     */
+    public int[] getNumberArray(final int[] array) throws IndexOutOfBoundsException{
+
+        return array;
+    }
+}
